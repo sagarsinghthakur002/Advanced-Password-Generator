7f79d832721bfd9053c91e00ce6c7031d78d33b8
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2024-09-24 18:26:24 +0545

    feat : adding funcation and usecallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react' 
+import { useCallback, useState } from 'react' 
 import './App.css'
 
 function App() {
@@ -6,12 +6,37 @@ function App() {
   const [numberAllowed, setnumberAllowed] = useState(false)
   const [charAllowed, setcharAllowed] = useState(false)
   const [Password, setpassword] = useState("")
+
+  const PasswordGenerator = useCallback(() => {
+    let password = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) {
+      str += "0123456789"
+    }
+    if (charAllowed) {
+      str += "!@#$%^&*()_+~`|}{[]:;?"
+    }
+
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random()* str.length +1 )
+      password += str[char]
+
+      }
+
+      setpassword(password)
+
+
+
+     [length, numberAllowed, charAllowed, setpassword]
+  })
   
 
   return (
     <>
-      <h1 className='text-4xl text-center
-       text-white'> Password generator</h1>
+      <div className='w-full max-w-md mx-auto shadow-md 
+      rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>
+        <div className='className="flex shadow rounded-lg overflow-hidden mb-4"'></div> 
+        </div>
     </>
   )
 }
